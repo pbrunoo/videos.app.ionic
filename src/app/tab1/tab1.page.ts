db95d2a6d502bf18a20fce6ffbb1056871024ca6
bruno <playideia@gmail.com>
2021-11-08 15:23:40 -0300

    adicionado array filmes e interface filme

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { IFilme } from '../models/iFilme.model';
 
 @Component({
   selector: 'app-tab1',
@@ -7,6 +9,50 @@ import { Component } from '@angular/core';
 })
 export class Tab1Page {
 
-  constructor() {}
+  titulo = 'Vídeos App';
+  listVideos: IFilme[] = [
+    {
+      nome: 'Eternos',
+      lancamento: '15/11/2021',
+      duracao: '2h 35m',
+      classificacao: 80,
+      generos: ['Ação', 'Aventura', 'Ficção científica'],
+      image: 'https://www.themoviedb.org/t/p/w440_and_h660_face/eDHOutMxLMgComnVhUK0Xfwunu5.jpg'
+    },
+    {
+      nome: 'Venon 2 ',
+      lancamento: '07/10/2021',
+      duracao: '1h 37m',
+      classificacao: 76,
+      generos: ['Ação', 'Aventura', 'Ficção científica'],
+      image: 'https://www.themoviedb.org/t/p/w600_and_h900_bestv2/h5UzYZquMwO9FVn15R2eK2itmHu.jpg'
+    }
+  ];
+
+  constructor(public alertController: AlertController) {}
+
+  async exibirAlertFavorit() {
+    const alert = await this.alertController.create({
+      header: 'Alerta!',
+      message: 'Message <strong>Deseja Favoritar o Filme?</strong>!!!',
+      buttons: [
+        {
+          text: 'Cancelar',
+          role: 'cancel',
+          cssClass: 'secondary',
+          handler: (blah) => {
+            console.log('Confirm Cancel: blah');
+          }
+        }, {
+          text: 'Sim, favoritar',
+          handler: () => {
+            console.log('Confirm Okay');
+          }
+        }
+      ]
+    });
+
+    await alert.present();
+  }
 
 }
